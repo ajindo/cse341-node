72f8c28ecb2aa6089deeeda157b9b41ad3939750
Atsushi Jindo <jin15001@byui.edu>
2021-01-12 22:35:44 -0700

    prove01

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localho
 
 const app = express();
 
-// Route setup. You can implement more in the future!
+// Team Activity
 const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03'); 
-const ta04Routes = require('./routes/ta04'); 
+const ta04Routes = require('./routes/ta04');
+// Prove Assignment 
 const pr01Routes = require('./routes/prove01');
 
 app.use(express.static(path.join(__dirname, 'public')))
